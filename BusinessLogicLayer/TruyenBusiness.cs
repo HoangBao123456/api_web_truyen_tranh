c3c2cd89031a79805a07f4100f3bc0d028a181e1
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-09-28 22:55:24 +0700

    Add files via upload

---
+++ BusinessLogicLayer/TruyenBusiness.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogicLayer;
+using DataAccessLayer;
+using DataModel;
+
+namespace BusinessLogicLayer
+{
+    public class TruyenBusiness : ITruyenBusiness
+    {
+        private ITruyenRepository _res;
+        public TruyenBusiness(ITruyenRepository res)
+        {
+            _res = res;
+        }
+        public bool Create(TruyenModel model)
+        {
+            return _res.Create(model);
+        }
+
+        public TruyenModel GetDatabyID(string id)
+        {
+            return _res.GetDatabyID(id);
+        }
+
+        public bool Update(TruyenModel model)
+        {
+            return _res.Update(model);
+        }
+        public bool Delete(TruyenModel model)
+        {
+            return _res.Delete(model);
+        }
+    }
+}
