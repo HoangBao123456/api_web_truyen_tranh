7988b22f17e0c2aef6c8ea78a9521719df8e5b67
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-09-28 23:03:28 +0700

    Add files via upload

---
+++ DataAccessLayer/TruyenRepository.cs
@@ -0,0 +1,93 @@
+﻿using DataModel;
+namespace DataAccessLayer
+{
+    public class TruyenRepository : ITruyenRepository
+    {
+        private IDatabaseHelper _dbHelper;
+        public TruyenRepository(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+        public TruyenModel GetDatabyID(string id)
+        {
+            string msgError = "";
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "get_by_id",
+                     "@id", id);
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+                return dt.ConvertTo<TruyenModel>().FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool Create(TruyenModel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "create",
+                "@id_truyen", model.id_truyen,
+                "@ma_truyen", model.ma_truyen,
+                "@ten_truyen", model.ten_truyen,
+                "@loai_truyen", model.loai_truyen,
+                "@tac_gia", model.tac_gia,
+                "@mo_ta", model.mo_ta
+                );
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool Update(TruyenModel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "truyen_update",
+                "@id_truyen", model.id_truyen,
+                "@ma_truyen", model.ma_truyen,
+                "@ten_truyen", model.ten_truyen,
+                "@loai_truyen", model.loai_truyen,
+                "@tac_gia", model.tac_gia,
+                "@mo_ta", model.mo_ta);
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public TruyenModel Delete(string id)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "truyen_delete",
+                     "@id", id);
+                if ((id != null && !string.IsNullOrEmpty(id.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(id) + msgError);
+                }
+                return;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/interfaces/ITruyenRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public partial interface ITruyenRepository
+    {
+        TruyenModel GetDatabyID(string id);
+        bool Create(TruyenModel model);
+        bool Update(TruyenModel model);
+        bool Delete(TruyenModel id);
+    }
+}
