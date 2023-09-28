4b4c6346034cb0ebe2f3eb97c4d906028fc4e4ef
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-09-28 23:02:40 +0700

    Add files via upload

---
+++ DataModel/TruyenModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DataModel
+{
+    public class TruyenModel
+    {
+        public int id_truyen { get; set; }
+        public string ma_truyen { get; set; }
+        public string ten_truyen { get; set; }
+        public string loai_truyen { get; set; }
+        public string tac_gia { get; set; }
+        public string mo_ta { get; set; }
+    }
+}
