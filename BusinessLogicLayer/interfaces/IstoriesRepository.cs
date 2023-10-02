f7ca21d90d666f4f76320dc4404d1c26444c4412
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-02 11:25:54 +0700

    Add files via upload

---
+++ BusinessLogicLayer/interfaces/IstoriesRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public partial interface IstoriesRepository
+    {
+        storiesModel GetDatabyName(string name);
+        bool Create(storiesModel model);
+        bool Update(storiesModel model);
+        bool Delete(string id);
+    }
+}
