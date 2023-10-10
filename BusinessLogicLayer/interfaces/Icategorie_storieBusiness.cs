afae549d9abe739defd366f33d36a10236557379
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-10 12:15:22 +0700

    Add files via upload

---
+++ BusinessLogicLayer/interfaces/Icategorie_storieBusiness.cs
@@ -0,0 +1,17 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public partial interface Icategorie_storieBusiness
+    {
+        categorie_storieModel GetDatabyid(string id);
+        bool Create(categorie_storieModel model);
+        bool Update(categorie_storieModel model);
+        bool Delete(string id);
+    }
+}
