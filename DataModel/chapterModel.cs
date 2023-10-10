6692c48d8418d94b66f6d64e19d44cb0c898ca82
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-10 12:17:30 +0700

    Add files via upload

---
+++ DataModel/chapterModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DataModel
+{
+    public class chapterModel
+    {
+        public string id { get; set; }
+        public string story_id { get; set; }
+        public string chapter { get; set; }
+        public string name { get; set; }
+        public string content { get; set; }
+
+    }
+}
---
+++ DataModel/starsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataModel
+{
+    public class starsModel
+    {
+        public string id { get; set; }
+        public string story_id { get; set; }
+        public string stars { get; set; }
+        public string count { get; set; }
+
+    }
+}
