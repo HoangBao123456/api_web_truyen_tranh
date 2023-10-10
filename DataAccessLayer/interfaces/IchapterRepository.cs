539fc37abd80ccafe307952b5264e98bdf73dd96
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-10 12:15:52 +0700

    Add files via upload

---
+++ DataAccessLayer/interfaces/IchapterRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public partial interface IchapterRepository
+    {
+        chapterModel GetDatabychapter(string chapter);
+        bool Create(chapterModel model);
+        bool Update(chapterModel model);
+        bool Delete(string id);
+    }
+}
