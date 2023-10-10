bd0ff652211d23cd2b517bda1be0d11b6ff76b3f
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-10 12:19:30 +0700

    Add files via upload

---
+++ web_truyen_tranh/Controllers/categorie_storieController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLogicLayer;
+using DataModel;
+using DataAccessLayer;
+
+namespace web_truyen_tranh.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class categorie_storieController : ControllerBase
+    {
+        private Icategorie_storieBusiness _categorieBusiness;
+        public categorie_storieController(Icategorie_storieBusiness categorie_storieBusiness)
+        {
+            _categorieBusiness = categorie_storieBusiness;
+        }
+        [Route("get_by-id/{id}")]
+        [HttpGet]
+        public categorie_storieModel GetDatabyid(string id)
+        {
+            return _categorieBusiness.GetDatabyid(id);
+        }
+        [Route("create-categorie_storie")]
+        [HttpPost]
+        public categorie_storieModel CreateItem([FromBody] categorie_storieModel model)
+        {
+            _categorieBusiness.Create(model);
+            return model;
+        }
+        [Route("update-categories_storie")]
+        [HttpPost]
+        public categorie_storieModel UpdateItem([FromBody] categorie_storieModel model)
+        {
+            _categorieBusiness.Update(model);
+            return model;
+        }
+        [Route("delete-categories_storie")]
+        [HttpPost]
+        public IActionResult DeleteItem(string id)
+        {
+            _categorieBusiness.Delete(id);
+            return Ok(new { messange = "xóa thành công" });
+        }
+    }
+}
---
+++ web_truyen_tranh/Controllers/chapterController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLogicLayer;
+using DataModel;
+using DataAccessLayer;
+
+namespace web_truyen_tranh.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class chapterController : ControllerBase
+    {
+        private IchapterBusiness _truyenBusiness;
+        public chapterController(IchapterBusiness truyenBusiness)
+        {
+            _truyenBusiness = truyenBusiness;
+        }
+        [Route("get_by-chapter/{chapter}")]
+        [HttpGet]
+        public chapterModel GetDatabychapter(string chapter)
+        {
+            return _truyenBusiness.GetDatabychapter(chapter);
+        }
+        [Route("create-chapter")]
+        [HttpPost]
+        public chapterModel CreateItem([FromBody] chapterModel model)
+        {
+            _truyenBusiness.Create(model);
+            return model;
+        }
+        [Route("update-chapter")]
+        [HttpPost]
+        public chapterModel UpdateItem([FromBody] chapterModel model)
+        {
+            _truyenBusiness.Update(model);
+            return model;
+        }
+        [Route("delete-chapter")]
+        [HttpPost]
+        public IActionResult DeleteItem(string id)
+        {
+            _truyenBusiness.Delete(id);
+            return Ok(new { messange = "xóa thành công" });
+        }
+    }
+}
