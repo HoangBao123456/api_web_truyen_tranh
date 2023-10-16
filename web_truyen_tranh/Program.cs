0a3966c666a0379bc9b9716a4e5a664ba1de4d1b
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-16 08:49:32 +0700

    Update Program.cs

--- web_truyen_tranh/Program.cs
+++ web_truyen_tranh/Program.cs
@@ -18,7 +18,18 @@ builder.Services.AddTransient<IstoriesBusiness, storiesBusiness>();
 builder.Services.AddTransient<IcategoriesRepository, categoriesRopository>();
 builder.Services.AddTransient<IcategoriesBusiness, categoriesBusiness>();
 
+builder.Services.AddTransient<Icategorie_storieRepository, categorie_storieRepository>();
+builder.Services.AddTransient<Icategorie_storieBusiness, categorie_storieBusiness>();
 
+
+builder.Services.AddTransient<IchapterRepository, chapterRepository>();
+builder.Services.AddTransient<IchapterBusiness, chapterBusiness>();
+
+builder.Services.AddTransient<IstarsRepository, starsRepository>();
+builder.Services.AddTransient<IstarsBusiness, starsBusiness>();
+
+builder.Services.AddTransient<Inew_storiesRepository, new_storiesRepository>();
+builder.Services.AddTransient<Inew_storiesBusiness, new_storiesBusiness>();
 
 // configure strongly typed settings objects
 IConfiguration configuration = builder.Configuration;
@@ -37,6 +48,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
@@ -45,3 +57,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
