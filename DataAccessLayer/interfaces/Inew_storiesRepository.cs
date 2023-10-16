1203d67682e43a7ce33408398ae99f62b8adf3be
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-16 09:10:49 +0700

    Add files via upload

---
+++ DataAccessLayer/interfaces/Inew_storiesRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public partial interface Inew_storiesRepository
+    {
+        List<new_storiesModel> GetData();
+        bool Create(new_storiesModel model);
+        bool Update(new_storiesModel model);
+        bool Delete(string id);
+    }
+}
