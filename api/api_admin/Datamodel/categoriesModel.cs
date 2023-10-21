02524bcf91b04d50e18f41404917ad8940b8494a
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-21 22:27:29 +0700

    ff

--- api/api_admin/Datamodel/categoriesModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataModel
-{
-    public class categoriesModel
-    {
-        public string id { get; set; }
-        public string name_categories { get; set; }
-        public string descr { get; set; }
-    }
-}
