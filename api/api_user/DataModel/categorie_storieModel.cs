8d2707ccb4adff152948e1030d9c88ccc04cce4c
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-10-27 00:11:55 +0700

    delete

--- api/api_user/DataModel/categorie_storieModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataModel
-{
-    public class categorie_storieModel
-    {
-        public string id { get; set; }
-        public string category_id { get; set; }
-        public string storie_id { get; set; }
-    }
-}
