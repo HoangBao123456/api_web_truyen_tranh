a4a5851032cde59488c1eb899f5c6e3183cd7d00
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-11-29 13:10:13 +0700

    Add files via upload

---
+++ api/api_admin/Datamodel/chapterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataModel
+{
+    public class chapterModel
+    {
+        public string id { get; set; }
+        public string story_id { get; set; }
+        public string chapter { get; set; }
+        public string name_chapter { get; set; }
+        public string content { get; set; }
+    }
+}
