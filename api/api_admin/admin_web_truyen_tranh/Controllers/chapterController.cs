d231092eae71f3ff426c1adae9884c0c5e0b2707
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-11-29 13:12:45 +0700

    Add files via upload

---
+++ api/api_admin/admin_web_truyen_tranh/Controllers/chapterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLogicLayer;
+using DataModel;
+using DataAccessLayer;
+
+namespace admin_web_truyen_tranh.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class chapterController : ControllerBase
+    {
+        private IchapterBusiness _truyenBusiness;
+        public chapterController(IchapterBusiness truyenBusiness)
+        {
+            _truyenBusiness = truyenBusiness;
+        }
+        [Route("create-chapter")]
+        [HttpPost]
+        public chapterModel CreateItem([FromBody] chapterModel model)
+        {
+            _truyenBusiness.Create(model);
+            return model;
+        }
+    }
+}
