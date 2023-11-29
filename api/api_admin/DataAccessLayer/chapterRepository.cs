2092c893226aed6890a6b11ecfe00427c8760044
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-11-29 13:11:21 +0700

    Add files via upload

---
+++ api/api_admin/DataAccessLayer/chapterRepository.cs
@@ -0,0 +1,34 @@
+﻿using DataModel;
+namespace DataAccessLayer
+{
+    public class chapterRepository : IchapterRepository
+    {
+        private IDatabaseHelper _dbHelper;
+        public chapterRepository(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+        public bool Create(chapterModel model)
+        {
+            string msgError = "";
+            try
+            {
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "create_chapter",
+                "@name", model.story_id,
+                "@chapter", model.chapter,
+                "@name_chapter", model.name_chapter,
+                "@content", model.content
+                );
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
