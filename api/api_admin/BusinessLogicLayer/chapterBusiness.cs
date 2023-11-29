012da23e9d5c6351f6670cdb41bb6c56ea9531b8
HoangBao123456 <90100197+HoangBao123456@users.noreply.github.com>
2023-11-29 13:12:17 +0700

    Add files via upload

---
+++ api/api_admin/BusinessLogicLayer/chapterBusiness.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogicLayer;
+using DataAccessLayer;
+using DataModel;
+
+namespace BusinessLogicLayer
+{
+    public class chapterBusiness : IchapterBusiness
+    {
+        private IchapterRepository _res;
+        public chapterBusiness(IchapterRepository res)
+        {
+            _res = res;
+        }
+        public bool Create(chapterModel model)
+        {
+            return _res.Create(model);
+        }
+    }
+}
